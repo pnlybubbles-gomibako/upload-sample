3346db3478a9846a1d47505ed2de0d2f5df2df00
pnlybubbles <pnlybubbles@gmail.com>
2018-04-17 12:02:16 +0900

    Replace koa-router with koa-route

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 const Koa = require('koa')
-const Router = require('koa-router')
+const router = require('koa-route')
 const serve = require('koa-static')
 const body = require('koa-body')
 const logger = require('koa-logger')
 const path = require('path')
 
 const app = new Koa()
-const router = new Router()
-
-router.post('/upload', (ctx, next) => {
-  console.log(ctx.request.body.files)
-  ctx.body = { status: 'success' }
-})
 
 app.use(body({
   multipart: true,
@@ -22,6 +16,9 @@ app.use(body({
 }))
 app.use(serve(path.resolve(__dirname, 'static')))
 app.use(logger())
-app.use(router.routes())
+app.use(router.post('/upload', ctx => {
+  console.log(ctx.request.body.files)
+  ctx.body = { status: 'success' }
+}))
 
 app.listen(4000)
